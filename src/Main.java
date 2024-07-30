d9ed23808fa6baac7e3854ff144c7121e8619084
ralvarado <ruben.alvarado@pucp.pe>
2024-07-30 12:10:07 -0500

    javero 7

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola");
-        System.out.println("mundo javero 6");
+        System.out.println("mundo javero 7");
         }
 }
